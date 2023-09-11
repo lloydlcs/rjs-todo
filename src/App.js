6297936244148e55a72e3259a08107f7485ec80d
Lloyd <lloyd_lcs1991@hotmail.com>
2023-09-11 15:53:10 +0800

    add layout and theme color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-// import { useState } from "react";
+import { useEffect } from "react";
 
 import { useTask } from "./contexts/task.context";
+import { useTheme } from "./contexts/theme.context";
 import SubmitForm from "./components/SubmitForm/SubmitForm";
 import TasksList from "./components/TasksList/TasksList";
+import Header from "./components/header/header";
 
 import './App.css';
+import styles from "./App.module.css";
 
 const App = () => {
     const { tasks, setTask } = useTask();
+    const { theme, setTheme } = useTheme();
+
+    useEffect(() => {
+        if (!theme) {
+            document.documentElement.setAttribute('data-theme', "blue");
+            setTheme("blue");
+        } else {
+            document.documentElement.setAttribute('data-theme', theme);
+        }
+    }, [theme, setTheme]);
 
     const addTask = (task) => {
         setTask(tasks.concat(task));
@@ -18,10 +31,36 @@ const App = () => {
         setTask(removedTask);
     };
 
+    const changeTheme = (theme) => {
+        document.documentElement.setAttribute('data-theme', theme);
+        setTheme(theme);
+    }
+
     return (
         <div className="App">
-            <SubmitForm addTask={addTask} />
-            <TasksList tasks={tasks} removeTask={removeTask} />
+            <Header />
+            <div className={ styles["tbody"] }>
+                <SubmitForm addTask={addTask} />
+                <TasksList tasks={tasks} removeTask={removeTask} />
+            </div>
+            <div className={ `d-flex ${styles["tfooter"]}` }>
+                <button type="button" className="tbutton py-2" onClick={ () => changeTheme('blue') }>
+                    <p className="m-0"><i className={ `bg-blue ${styles["tbox"]}` } /></p>
+                    <p className="m-0 c-blue">蓝色</p>
+                </button>
+                <button type="button" className="tbutton py-2" onClick={ () => changeTheme('purple') }>
+                    <p className="m-0"><i className={ `bg-purple ${styles["tbox"]}` } /></p>
+                    <p className="m-0 c-purple">紫色</p>
+                </button>
+                <button type="button" className="tbutton py-2" onClick={ () => changeTheme('green') }>
+                    <p className="m-0"><i className={ `bg-green ${styles["tbox"]}` } /></p>
+                    <p className="m-0 c-green">绿色</p>
+                </button>
+                <button type="button" className="tbutton py-2" onClick={ () => changeTheme('orange') }>
+                    <p className="m-0"><i className={ `bg-orange ${styles["tbox"]}` } /></p>
+                    <p className="m-0 c-orange">橙色</p>
+                </button>
+            </div>
         </div>
     );
 };
